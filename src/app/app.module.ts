7597a8fceb7165046865ed316f76ad537604bc0e
227Mahaman <zalkiabass.456@gmail.com>
2020-06-28 23:09:20 +0100

    Création et intégration(dans app.module) du service d'authentification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
 import { AppareilComponent } from './appareil/appareil.component';
-import  { AppareilService } from "./services/appareil.service";
+import  { AppareilService } from './services/appareil.service';
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
-import {RouterModule, Routes} from "@angular/router";
+import {RouterModule, Routes} from '@angular/router';
+import {AuthService} from './services/auth.service';
 
 const appRoutes: Routes = [
   //Objet et son component
@@ -36,7 +37,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
-    AppareilService
+    AppareilService,
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
