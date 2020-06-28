232d8f140547e27225f555254bd4c17b9bb0e3f0
227Mahaman <zalkiabass.456@gmail.com>
2020-06-28 17:36:13 +0100

    Fonction onAllumer()update et add onEteindre utilisant le service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AppareilService} from './services/appareil.service';
 
 @Component({
@@ -6,7 +6,7 @@ import {AppareilService} from './services/appareil.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   isAuth = false;
 
   lastUpdate = new Promise(
@@ -20,20 +20,7 @@ export class AppComponent {
     }
   );
 
-  appareils = [
-    {
-      name: 'Machine à laver',
-      status: 'éteint'
-    },
-    {
-      name: 'Télévision',
-      status: 'allumé'
-    },
-    {
-      name: 'Ordinateur',
-      status: 'éteint'
-    }
-  ];
+  appareils: any[];
 
   constructor( private appareilService: AppareilService) {
     setTimeout(
@@ -42,7 +29,16 @@ export class AppComponent {
       }, 4000
     );
   }
+
+  ngOnInit() {
+    this.appareils = this.appareilService.appareils;
+  }
+
   onAllumer() {
-    console.log('On allume tout !');
+    this.appareilService.switchOnAll();
+  }
+
+  onEteindre() {
+    this.appareilService.switchOffAll();
   }
 }
