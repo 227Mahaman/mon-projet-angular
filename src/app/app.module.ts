a93154d71e4672267d6b52b889e7162976ccb17c
227Mahaman <zalkiabass.456@gmail.com>
2020-06-29 00:49:15 +0100

    Acceder au page détail de chaque appareil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,12 @@ import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import {RouterModule, Routes} from '@angular/router';
 import {AuthService} from './services/auth.service';
+import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
 
 const appRoutes: Routes = [
   //Objet et son component
   { path: 'appareils', component: AppareilViewComponent },
+  { path: 'appareils/:id', component: SingleAppareilComponent },
   { path: 'auth', component: AuthComponent },
   //Page d'accueil
   { path: '', component: AppareilViewComponent }
@@ -26,7 +28,8 @@ const appRoutes: Routes = [
     MonPremierComponent,
     AppareilComponent,
     AuthComponent,
-    AppareilViewComponent
+    AppareilViewComponent,
+    SingleAppareilComponent
   ],
   imports: [
     BrowserModule,
