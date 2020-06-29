6b75286c22eab3739d693ab560f43d87866e7b49
227Mahaman <zalkiabass.456@gmail.com>
2020-06-29 18:25:51 +0100

    Redirection si la page n'existe pas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import {RouterModule, Routes} from '@angular/router';
 import {AuthService} from './services/auth.service';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { FourOfFourComponent } from './four-of-four/four-of-four.component';
 
 const appRoutes: Routes = [
   //Objet et son component
@@ -19,7 +20,9 @@ const appRoutes: Routes = [
   { path: 'appareils/:id', component: SingleAppareilComponent },
   { path: 'auth', component: AuthComponent },
   //Page d'accueil
-  { path: '', component: AppareilViewComponent }
+  { path: '', component: AppareilViewComponent },
+  { path: 'not-found', component: FourOfFourComponent },
+  { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
@@ -29,7 +32,8 @@ const appRoutes: Routes = [
     AppareilComponent,
     AuthComponent,
     AppareilViewComponent,
-    SingleAppareilComponent
+    SingleAppareilComponent,
+    FourOfFourComponent
   ],
   imports: [
     BrowserModule,
